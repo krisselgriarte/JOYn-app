b06ada0db6a907e230c67f40fa79ca597672dc7c
cbaddeley <corybaddeley@gmail.com>
2017-07-08 11:37:03 -0400

    location and activity form with js files

---
+++ assets/javascript/ajaxcall.js
@@ -0,0 +1,62 @@
+var latitude;
+var longitude;
+var ajaxLimit;
+var activity;
+var city;
+var state;
+var country;
+var radius;
+
+
+function createAjaxUrl(lat, limit, lon, activity, city, country, state, radius) {
+	var baseUrl = "https://trailapi-trailapi.p.mashape.com/?";
+	var params = "";
+	if (lat && lon) {
+		params += "lat=" + lat + "&lon=" + lon + "&";
+	} else if (city && country && state) {
+		params += "q[city_cont]=" + city + "&q[country_cont]=" + country + "&q[state_cont]=" + state + "&";
+	}
+	if (limit) {
+		params += "limit=" + limit + "&";
+	}
+	if (activity) {
+		params += "q[activities_activity_type_name_eq]=" + activity + "&";
+	}
+	if (radius) {
+		params += "radius=" + radius;
+	}
+	var fullUrl = baseUrl + params;
+	var url = fullUrl.replace(/ /g, "+");
+	return url;
+}
+
+function trailAjaxCall(url) {
+	$.ajaxSetup({
+	    headers: { 'X-Mashape-Key': '1WWcaF7KgRmshaVdzjMEkHd4xRUWp1NbhqVjsnVdjN3Sz5OMgU' }
+	});
+
+		$.ajax({
+			url: url,
+			method: "GET",
+			beforeSend: function(jqXHR, settings) {
+      document.write(settings.url);
+  }
+		}).done(function(response) {
+			console.log(response);
+		})
+}
+
+$( document ).ready(function() {
+    latitude = parseFloat(sessionStorage.getItem("latitude"));
+    longitude = parseFloat(sessionStorage.getItem("longitude"));
+    activity = (sessionStorage.getItem("activity")).toLowerCase();
+    city = sessionStorage.getItem("city");
+    state = sessionStorage.getItem("state");
+    country = sessionStorage.getItem("country");
+    radius = parseInt(sessionStorage.getItem("radius"));
+
+    console.log(latitude);
+
+     var url = createAjaxUrl(latitude, ajaxLimit, longitude, activity, city, country, state, radius);
+     trailAjaxCall(url);
+});
---
+++ assets/javascript/locationActivity.js
@@ -0,0 +1,58 @@
+$("#activitySelect").bind("change", function () {
+    $("#subOutdoors").toggle(this.value == 1);
+}).change();
+
+$("#radiusInput").slider({
+	tooltip: 'always'
+});
+
+$("#locationButton").on("click", function() {
+	var options = {
+  	enableHighAccuracy: true,
+  	timeout: 5000,
+  	maximumAge: 0
+	};
+
+	function success(pos) {
+  		var crd = pos.coords;
+  		sessionStorage.clear();
+  		sessionStorage.setItem("latitude", crd.latitude);
+      	sessionStorage.setItem("longitude", crd.longitude);
+      	sessionStorage.setItem("country", "");
+    	sessionStorage.setItem("state", "");
+    	sessionStorage.setItem("city", "");
+  		$(location).attr('href', 'something.html');
+	};
+
+	function error(err) {
+  		console.warn(`ERROR(${err.code}): ${err.message}`);
+	};
+
+navigator.geolocation.getCurrentPosition(success, error, options);
+
+});
+
+$("#locationFormSubmit").on("click", function() {
+	event.preventDefault();
+
+	sessionStorage.clear();
+
+	sessionStorage.setItem("country", $("#countryId").val());
+    sessionStorage.setItem("state", $("#stateId").val());
+    sessionStorage.setItem("city", $("#cityId").val());
+    sessionStorage.setItem("latitude", "");
+    sessionStorage.setItem("longitude", "");
+	
+	$(location).attr('href', 'activitySelect.html');
+	return false;
+});
+
+$("#activitySubmit").on("click", function() {
+	event.preventDefault();
+
+	sessionStorage.setItem("radius", $("#radiusInput").val());
+    sessionStorage.setItem("activity", $("#subOutdoorsSelect").val());
+	
+	$(location).attr('href', 'results.html');
+	return false;
+});
