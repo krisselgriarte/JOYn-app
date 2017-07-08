29bcdfdb62c48e777cb2dc8e802c50cc0d526a6a
Daniel <dskay3@gmail.com>
2017-07-08 18:02:08 -0400

    Added functionality for the Yes Button to link to Firebase.

--- assets/javascript/db.js
+++ assets/javascript/db.js
@@ -10,15 +10,23 @@ var users = {
 }
 
 
-$(".btn-info").on("click", function (event) {
-    users.name = firebase.auth().currentUser.email;
-    users.userID = firebase.auth().currentUser.uid;
-    db.ref().push(users);
-});
-
 function storage(response) {
-    users.name = firebase.auth().currentUser.email;
-    users.userID = firebase.auth().currentUser.uid;
-    db.ref().push(users);
-    console.log("hi");
-};
+    $(".btn-yes").on("click", function (event) {
+        users.name = firebase.auth().currentUser.email;
+        users.userID = firebase.auth().currentUser.uid;
+
+        var isValid = true;
+        // db.ref().on("child_added", function (snapshot) {
+
+        for (var i = 0; i < yesList.length; i++) {
+            if (yesList[i] == $(this).parent()[0].attributes[5].value) {
+                isValid = false;
+            }
+        }
+
+        if (isValid) {
+            yesList.push($(this).parent()[0].attributes[5].value);
+            db.ref().push(users);
+        }
+    });
+}
